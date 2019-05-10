b97e7259b2cefe3e90e7b7375c3f13d477178d89
runcycle <justinbartholomew@gmail.com>
2019-05-10 16:49:00 -0400

    Added changes to main.dart, option.dart, and terminal.dart

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,21 @@
 import 'package:prompter_jcb/src/terminal.dart';
+import 'package:prompter_jcb/src/option.dart';
+
 
 void main() {
-  print('Hi there!');
+  var terminal = new Terminal();
+
+  var options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f')
+  ];
+
+  // terminal.clearScreen();
+  // terminal.printPrompt('Hi There!');
+  // var input = terminal.collectInput();
+  // print('You just entered: $input');
+
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  print(response);
 }
